702cda1be5c5d6313480b43e36eaffdea613760a
Nadaka Yusuke <ndkysk@gmail.com>
2025-09-14 16:54:19 +0900

    feat:add Contact model, request validation, views and styles

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
+use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
 {
@@ -12,9 +14,27 @@ class ContactController extends Controller
     //     return view('index');
     // }
 
+    //初期画面表示
     public function index()
-{
-    return view('index');
-}
+    {
+        return view('index');
+    }
 
+    //確認画面表示
+    public function confirm(ContactRequest $request){
+        $contact = $request->only(['name', 'email', 'tel','content']);
+        // return $contact; レスポンスボディーに返す
+        // return view('confirm');　ビューを呼び出す
+        // return view('confirm', ['contact' => $contact]);　ビューを呼び出し、値を渡す
+        return view('confirm', compact('contact'));  //compactでシンプルに
+    }
+
+    //登録完了
+    public function store(ContactRequest $request){
+        $contact = $request->only(['name', 'email', 'tel','content']);
+        // return view('thanks');　ビューを呼び出す
+        // return view('thanks', compact('contact'));  //compactでシンプルに
+        Contact::create($contact); //データ登録
+        return view('thanks');
+    }
 }
---
+++ src/database/migrations/2025_09_14_144148_create_contacts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('email', 255);
+            $table->string('tel', 11);
+            $table->text('content')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
